3691942607e155a6f596066997da29b2870b84ba
MS-Marcks <gersonramos46@gmail.com>
2023-09-15 12:53:11 -0600

    se agrego vercel

--- index.js
+++ index.js
@@ -418,7 +418,7 @@ const server = net.createServer(async (socket) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
