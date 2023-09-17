4985996ff354ff345cfa4ecd83794e1396614f65
MS-Marcks <gersonramos46@gmail.com>
2023-09-17 09:26:06 -0600

    prueba 6 de net

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const net = require('net');
 
 const server = net.createServer((socket) => {
     console.log('Cliente conectado');
-
+    //socket.connect();
+    console.log("===========================", socket.writable);
     if (socket.writable) {
-        socket.write('¡Bienvenido al servidor de texto!\r\n');
-        socket.write('Escribe "exit" para salir.\r\n\r\n');
+        socket.write("hola como estan");
+        /*socket.write('¡Bienvenido al servidor de texto!\r\n');
+        socket.write('Escribe "exit" para salir.\r\n\r\n');*/
+
     } else {
         console.log('El socket no está disponible para escritura.');
     }
